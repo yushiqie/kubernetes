a346bc8e852bb0abce49d2ef920d1479c85910ef
Erik Wilson <Erik.E.Wilson@gmail.com>
2019-04-15 09:30:32 -0700

    Ignore proxy settings for kubelet client
    Proxy settings may interfere with a kubelet client communicating with
    the API server, so set the proxy to nil.

--- pkg/kubelet/client/kubelet_client.go
+++ pkg/kubelet/client/kubelet_client.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"net/url"
 	"strconv"
 	"time"
 
@@ -58,6 +59,9 @@ type KubeletClientConfig struct {
 
 	// Lookup will give us a dialer if the egress selector is configured for it
 	Lookup egressselector.Lookup
+
+	// Proxy is a custom proxy function for the client
+	Proxy func(*http.Request) (*url.URL, error)
 }
 
 // ConnectionInfo provides the information needed to connect to a kubelet
@@ -116,6 +120,7 @@ func makeTransport(config *KubeletClientConfig, insecureSkipTLSVerify bool) (htt
 		rt = utilnet.SetOldTransportDefaults(&http.Transport{
 			DialContext:     dialer,
 			TLSClientConfig: tlsConfig,
+			Proxy:           config.Proxy,
 		})
 	}
 
